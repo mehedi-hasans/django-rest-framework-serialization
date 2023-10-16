11332072892669e88b0f1b0d19ad6c8da9e87e3b
mehedi <mehedi4595@gmail.com>
2023-10-16 11:40:00 +0600

    Create Model, serializers.py, Serializer class

---
+++ snippets/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Snippet(models.Model):
+    name = models.CharField(max_length=30)
+    dep = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+
+    def __str__(self):
+        return self.name
