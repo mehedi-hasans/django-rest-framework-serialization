e6642680c71354a5ca56526558de5fc8a66bc983
mehedi <mehedi4595@gmail.com>
2023-10-16 12:09:08 +0600

    Create Using ModelSerializers, Edit snippets/views.py, Crate snippets/urls.py

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,20 +3,7 @@ from snippets.models import Snippet
 
 
 
-class SnippetSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=30)
-    dep = serializers.CharField(max_length=30)
-    email = serializers.EmailField(max_length=30)
-
-
-    def create(self, validated_data):
-        return Snippet.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['name', 'dep', 'email']
