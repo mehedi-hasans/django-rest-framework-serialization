64a6712f28f402fc3c6f864507aa29e824e2ebb8
mehedi <mehedi4595@gmail.com>
2023-10-17 10:12:22 +0600

    Retrieve, update or delete a snippet instance using id search data

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,5 +3,5 @@ from snippets import views
 
 urlpatterns = [
     path('api/', views.SnippetList.as_view()),
-    # path('apid/<int:pk>/', views.snippet_detail),
+    path('api/<int:pk>/', views.SnippetDetail.as_view()),
 ]
