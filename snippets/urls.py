85035d26b925e157a8c0b76de555ffb5741cc149
mehedi <mehedi4595@gmail.com>
2023-10-17 09:50:54 +0600

    Retrieve, update or delete a code snippet using id search data

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,5 +3,5 @@ from snippets import views
 
 urlpatterns = [
     path('api/', views.snippet_list),
-    # path('snippets/<int:pk>/', views.snippet_detail),
+    path('apid/<int:pk>/', views.snippet_detail),
 ]
